a9b4458469bff2fcf288b41b53d9ab2567a0dbdb
Chain Qian <chainz@foxmail.com>
2017-09-21 23:36:10 +0800

    update

--- chain/test01/TreeTest.java
+++ chain/test01/TreeTest.java
@@ -1,4 +1,6 @@
-package com.chain.javase.algorithm.day01;
+package com.chain.algorithm.test.day01;
+
+import java.util.List;
 
 import org.junit.Test;
 
@@ -33,6 +35,37 @@ public class TreeTest {
 		System.out.println(tree.preOrderToList());
 		System.out.println(tree.inOrderToList());
 		System.out.println(tree.postOrderToList());
+
+		// 二叉树的中序遍历输出的即是已经排序好的数组
+		// 要求：查找二叉排序树的最接近k的节点
+
+		int k = 100;
+		List<Integer> list = tree.inOrderToList();
+
+		int value = getClosest(list, k);
+		System.out.println(value);
+	}
+
+	// 查找二叉排序树的最接近k的节点
+	private int getClosest(List<Integer> list, int k) {
+		int size = list.size();
+		for (int i = 0; i < size; i++) {
+			int current = list.get(i);
+			if (current == k) {
+				return current;
+			} else if (current > k) {
+				if (i > 0) {
+					int before = list.get(i - 1);
+					int d1 = current - k;
+					int d2 = k - before;
+					// 如果相等的话就返回排在前面的值
+					return d2 > d1 ? current : before;
+				} else {
+					return current;
+				}
+			}
+		}
+		return list.get(size - 1);
 	}
 
 }
